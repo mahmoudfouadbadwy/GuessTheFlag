bb78d288bb71093627de90c8a97abc2c40bec182
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2021-05-16 15:44:02 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mahmoud Fouad on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
